638953023cf61e275ecea0a970e75736b18c21da
orhanus <orhanus@maestralsolutions.com>
2021-10-05 10:30:12 +0200

    Add project files.

---
+++ Common/Enums/MediaType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Common.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum MediaType
+    {
+        All = 0,
+        Movie = 1,
+        Show = 2  
+    }
+}
---
+++ Core/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Common.Enums;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Entities
+{
+    [Table("Users")]
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public Role UserRole { get; set; }
+        public ICollection<Screening> Screenings { get; set; } = new List<Screening>();
+    }
+}
